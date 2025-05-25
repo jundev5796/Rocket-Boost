bb7d7f37c09df714186cd1a2de9c18a7584cb89b
hyunjunbaek <hjbaek5796@gmail.com>
2025-05-25 17:31:38 +0900

    Test movement input

---
+++ Rocket-Boost/Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] InputAction thrust;
+
+    private void OnEnable()
+    {
+        thrust.Enable();
+    }
+
+    private void Update()
+    {
+        if (thrust.IsPressed())
+            Debug.Log("Thrust is pressed!");
+    }
+}
