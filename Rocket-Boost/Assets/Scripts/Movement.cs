56083cb9083ae50558fe5fd3146df0d3d86d1548
hyunjunbaek <hjbaek5796@gmail.com>
2025-05-28 13:54:01 +0900

    Test rotation input

--- Rocket-Boost/Assets/Scripts/Movement.cs
+++ Rocket-Boost/Assets/Scripts/Movement.cs
@@ -6,6 +6,7 @@ public class Movement : MonoBehaviour
     Rigidbody rb;
 
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
     [SerializeField] float thrustStrength = 100f;
 
     private void Start()
@@ -16,13 +17,26 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     private void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
     {
         if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        Debug.Log(rotationInput);
     }
 }
